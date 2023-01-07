8fb545b9456531a79e4431dc722c9c41258aafcf
xz <xz@qq.com>
2023-01-07 19:44:54 +0800

    fix(core): npm package error

--- src/index.js
+++ src/index.js
@@ -4,14 +4,3 @@ import './components/index.css'
 import './elements/index.css'
 import './form/index.css'
 import './helpers/index.css'
-
-const messageWrap = document.querySelector('.message-wrap')
-const message = document.querySelector('.message')
-
-messageWrap.addEventListener('click', (e) => {
-  message.classList.toggle('is-active')
-
-  setTimeout(() => {
-    message.classList.remove('is-active')
-  }, 3000)
-})
